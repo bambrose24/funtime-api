15c246f63d9dab0a7f5bf3df464c449dffe724db
Bob Ambrose <bambrose24@gmail.com>
2022-09-04 21:44:49 -0400

    cron updates the week

--- src/cron/keepThingsUpdated.ts
+++ src/cron/keepThingsUpdated.ts
@@ -1,9 +1,10 @@
 import { getGamesBySeason } from "@shared/mysportsfeeds";
 import moment from "moment";
+import updateGamesAndPicks from "./updateGamesAndPicks";
 export default async function keepThingsUpdated() {
   console.log("cron is running at ", moment().toString());
 
   const games = await getGamesBySeason(2022);
 
-  // call updateGamesAndPicks when that's done
+  await updateGamesAndPicks(games);
 }
