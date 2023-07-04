77f7fe300132770d23a8b84ef92253005fab9fb3
Bob Ambrose <bambrose24@gmail.com>
2023-07-04 18:57:12 -0400

    start on 0s

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,10 @@ import logger from 'jet-logger';
 import server from './server';
 import config from './config';
 
-const port = config.port || 3000;
+const port = process.env.PORT || config.port || 3000;
 const serverStartMsg = `🚀 Express server started on port: ${port} (node version ${process.version})`;
 
 // Start server
-server.listen(port, () => {
+server.listen(typeof port === 'string' ? parseInt(port) : port, '0.0.0.0', () => {
   logger.info(serverStartMsg);
 });
