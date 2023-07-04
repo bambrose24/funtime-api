6cbc4db7447678563c240dc0dc235358c8097b23
Bob Ambrose <bambrose24@gmail.com>
2023-07-03 22:42:19 -0400

    unique share_code on leagues

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,37 @@
+/* eslint-disable no-undef */
 module.exports = {
   env: {
     browser: true,
     es2021: true,
   },
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
-  overrides: [],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:type-graphql/recommended',
+  ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint'],
+  plugins: ['@typescript-eslint', 'type-graphql'],
   rules: {},
+  overrides: [
+    {
+      files: ['**/*.graphql'],
+      parser: '@graphql-eslint/eslint-plugin',
+      extends: 'plugin:@graphql-eslint/schema-recommended',
+      rules: {
+        '@graphql-eslint/require-description': 'off',
+        '@graphql-eslint/strict-id-in-types': 'off',
+        '@graphql-eslint/naming-convention': 'off',
+        '@graphql-eslint/no-hashtag-description': 'off',
+        '@graphql-eslint/no-unreachable-types': 'off',
+        '@graphql-eslint/description-style': 'off',
+        '@graphql-eslint/no-typename-prefix': 'off',
+      },
+    },
+  ],
 };
