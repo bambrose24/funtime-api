7be838ad047fad821e344f68af7a8d66f2247ac0
Bob Ambrose <bambrose24@gmail.com>
2023-08-30 23:22:38 -0400

    comment out user update script in case

--- src/scripts/update-user-data.ts
+++ src/scripts/update-user-data.ts
@@ -1,13 +1,13 @@
 import datastore from '@shared/datastore';
 
 async function run() {
-  const members = await datastore.leagueMember.findMany({where: {league_id: 8}});
-  const memberIds = members.map(m => m.membership_id);
-  const users = await datastore.user.findMany({
-    where: {leaguemembers: {some: {membership_id: {in: memberIds}}}},
-  });
-
-  await datastore.leagueMember.update({where: {membership_id: 378}, data: {role: 'admin'}});
+  // const members = await datastore.leagueMember.findMany({where: {league_id: 8}});
+  // const memberIds = members.map(m => m.membership_id);
+  // const users = await datastore.user.findMany({
+  //   where: {leaguemembers: {some: {membership_id: {in: memberIds}}}},
+  // });
+  // erica 2023 member update
+  // await datastore.leagueMember.update({where: {membership_id: 386}, data: {role: 'admin'}});
 }
 
 run();
