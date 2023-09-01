2d26522ea0c642b61cf8a3f672a39ac63302ec35
Bob Ambrose <bambrose24@gmail.com>
2023-08-31 22:30:26 -0400

    more custom resolvers

--- src/graphql/queries/user/id.ts
+++ src/graphql/queries/user/id.ts
@@ -1,11 +1,42 @@
-import {FieldResolver, Resolver, Root, ID} from 'type-graphql';
+import {FieldResolver, Resolver, Root, ID, Int, Arg} from 'type-graphql';
 import * as TypeGraphQL from '@generated/type-graphql';
-import {User} from '@prisma/client';
+import {League, LeagueMember, User} from '@prisma/client';
+import datastore from '@shared/datastore';
 
 @Resolver(() => TypeGraphQL.User)
 export default class UserID {
   @FieldResolver(_type => ID)
   async id(@Root() user: User): Promise<string> {
     return user.uid.toString();
+  }
+
+  @FieldResolver(_type => TypeGraphQL.League, {nullable: true})
+  async league(
+    @Root() user: User,
+    @Arg('league_id', () => Int)
+    league_id: number
+  ): Promise<League | null> {
+    const membership = await datastore.leagueMember.findFirst({
+      where: {league_id, user_id: user.uid},
+    });
+    if (!membership) {
+      return null;
+    }
+    return await datastore.league.findFirst({where: {league_id}});
+  }
+
+  @FieldResolver(_type => TypeGraphQL.LeagueMember, {nullable: true})
+  async leagueMember(
+    @Root() user: User,
+    @Arg('league_id', () => Int)
+    league_id: number
+  ): Promise<LeagueMember | null> {
+    const membership = await datastore.leagueMember.findFirst({
+      where: {league_id, user_id: user.uid},
+    });
+    if (!membership) {
+      return null;
+    }
+    return membership ?? null;
   }
 }
