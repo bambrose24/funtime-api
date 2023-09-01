43c053097e353578d7dc362e3e5837227ae1d40d
Bob Ambrose <bambrose24@gmail.com>
2023-08-31 21:49:15 -0400

    identify users on sentry

--- src/shared/auth/index.ts
+++ src/shared/auth/index.ts
@@ -2,6 +2,7 @@ import httpContext from 'express-http-context';
 import {createClient} from '@supabase/supabase-js';
 import {setToken, setUser} from './user';
 import datastore from '@shared/datastore';
+import * as Sentry from '@sentry/node';
 
 export const supabase = createClient(
   process.env.SUPABASE_URL || '',
@@ -14,6 +15,19 @@ export async function authorizeAndSetSupabaseUser(token: string): Promise<void> 
     setToken(token);
     if (user.data.user) {
       const dbUser = await datastore.user.findFirst({where: {email: user.data.user.email}});
+      if (dbUser) {
+        Sentry.setUser({
+          id: dbUser.uid,
+          email: dbUser.email,
+          username: dbUser.username,
+        });
+      } else {
+        Sentry.setUser({
+          id: user.data.user.id,
+          email: user.data.user.email,
+        });
+      }
+
       setUser({supabase: user.data.user, dbUser});
     }
   } catch (e) {
