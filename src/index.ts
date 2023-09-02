dcfc5b549e62dfcec37a5bea8ac80c5c2103c170
Bob Ambrose <bambrose24@gmail.com>
2023-09-02 10:29:48 -0400

    env

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,10 @@ import 'module-alias/register';
 import 'reflect-metadata';
 import logger from 'jet-logger';
 import server from './server';
-import config from './config';
+import config, {env} from './config';
 
 const port = process.env.PORT || config.port || 3000;
-const serverStartMsg = `🚀 Express server started on port: ${port} (node version ${process.version})`;
+const serverStartMsg = `🚀 Express server started on port: ${port} (node version ${process.version}) (env ${env})`;
 
 // Start server
 if (process.env.FUNTIME_RUN_SERVER === 'true') {
