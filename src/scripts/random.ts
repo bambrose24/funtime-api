4957044579dc98e381dacbaba517a4f1cd314886
Bob Ambrose <bambrose24@gmail.com>
2023-09-10 21:09:11 -0400

    random script 2

--- src/scripts/random.ts
+++ src/scripts/random.ts
@@ -1,7 +1,7 @@
 import datastore from '@shared/datastore';
 
 async function run() {
-  await datastore.pick.updateMany({data: {done: 1}, where: {leaguemembers: {league_id: {lte: 7}}}});
+  // await datastore.pick.updateMany({data: {done: 1}, where: {leaguemembers: {league_id: {lte: 7}}}});
 }
 
 run();
