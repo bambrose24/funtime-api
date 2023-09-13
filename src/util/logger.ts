5f97baa3192df27317a2039fdf8f9809486907c0
Bob Ambrose <bambrose24@gmail.com>
2023-09-13 10:31:40 -0400

    getDefaultLoggingMeta

--- src/util/logger.ts
+++ src/util/logger.ts
@@ -2,6 +2,7 @@ import {env} from '../../src/config';
 import winston, {createLogger, format} from 'winston';
 import {WinstonTransport as AxiomTransport} from '@axiomhq/winston';
 import {Console} from 'winston/lib/winston/transports';
+import {getUser} from '@shared/auth/user';
 
 const axiomTransport = new AxiomTransport({
   dataset: 'funtime_api_logs', // defaults to process.env.AXIOM_DATASET
@@ -14,6 +15,25 @@ const transports = env === 'production' ? [axiomTransport] : [axiomTransport];
 const cronOrServer: 'cron' | 'server' =
   process.env.FUNTIME_RUN_SERVER === 'true' ? 'server' : 'cron';
 
+function getDefaultLoggingMeta() {
+  const meta: Record<any, any> = {service: 'funtime-api', env, serverApp: cronOrServer};
+
+  try {
+    const user = getUser();
+    if (user?.dbUser) {
+      meta.userId = user.dbUser.uid;
+      meta.email = user.dbUser.email;
+    } else if (user?.supabase) {
+      if (user.supabase.email) {
+        meta.email = user.supabase.email;
+      }
+    }
+  } catch (e) {
+    // cant use logger lol
+  }
+  return meta;
+}
+
 export const logger = createLogger({
   level: 'info',
   format: format.combine(
@@ -24,7 +44,7 @@ export const logger = createLogger({
     format.splat(),
     format.json()
   ),
-  defaultMeta: {service: 'funtime-api', env, serverApp: cronOrServer},
+  defaultMeta: getDefaultLoggingMeta(),
   transports: transports,
   exceptionHandlers: transports,
   rejectionHandlers: transports,
