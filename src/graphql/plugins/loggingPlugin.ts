754a1c3c7072e5eeec8fce12b4e23875d062eeaf
Bob Ambrose <bambrose24@gmail.com>
2023-09-12 23:45:02 -0400

    maybe logger plugin working

--- src/graphql/plugins/loggingPlugin.ts
+++ src/graphql/plugins/loggingPlugin.ts
@@ -6,38 +6,14 @@ export const loggingPlugin: ApolloServerPlugin<ApolloContext> = {
   requestDidStart({request, contextValue}) {
     return new Promise(resolve => {
       if (request.operationName) {
-        logger.info(`[graphql] Running operation ${request.operationName}`);
+        logger.info(`[graphql] Running operation ${request.operationName}`, {
+          operationName: request.operationName,
+          query: request.query,
+          variables: request.variables,
+        });
       }
 
-      resolve({
-        async willSendResponse(requestContext) {
-          if (requestContext.errors) {
-            logger.error(
-              `[graphql] Error running ${requestContext.operationName}: ${JSON.stringify(
-                requestContext.errors
-              )}`,
-              requestContext
-            );
-          }
-          logger.info(`[graphql] Finished running ${requestContext.operationName}`, requestContext);
-          contextValue.transaction.finish();
-        },
-        // executionDidStart() {
-        //   return new Promise(resolveExecution => {
-        //     resolveExecution({
-        //       willResolveField({contextValue, info}) {
-        //         const span = contextValue.transaction.startChild({
-        //           op: 'resolver',
-        //           description: `${info.parentType.name}.${info.fieldName}`,
-        //         });
-        //         return () => {
-        //           span.finish();
-        //         };
-        //       },
-        //     });
-        //   });
-        // },
-      });
+      resolve({});
     });
   },
 };
--- src/util/logger.ts
+++ src/util/logger.ts
@@ -1,6 +1,7 @@
 import {env} from '../../src/config';
-import {createLogger, format} from 'winston';
+import winston, {createLogger} from 'winston';
 import {WinstonTransport as AxiomTransport} from '@axiomhq/winston';
+import {Console} from 'winston/lib/winston/transports';
 
 const axiomTransport = new AxiomTransport({
   dataset: 'funtime_api_logs', // defaults to process.env.AXIOM_DATASET
@@ -8,18 +9,30 @@ const axiomTransport = new AxiomTransport({
   orgId: 'funtime-ywin', // defaults to process.env.AXIOM_ORG_ID
 });
 
+const transports = env === 'production' ? [axiomTransport] : [new Console()];
+
+// export const logger = createLogger({
+//   level: 'info',
+//   format: format.combine(
+//     format.timestamp({
+//       format: 'YYYY-MM-DD HH:mm:ss',
+//     }),
+//     format.errors({stack: true}),
+//     format.splat(),
+//     format.json()
+//   ),
+//   defaultMeta: {service: 'funtime-api', env},
+//   transports: transports,
+//   exceptionHandlers: transports,
+//   rejectionHandlers: transports,
+// });
+
 export const logger = createLogger({
   level: 'info',
-  format: format.combine(
-    format.timestamp({
-      format: 'YYYY-MM-DD HH:mm:ss',
+  format: winston.format.json(),
+  transports: [
+    new winston.transports.File({
+      filename: 'info.log',
     }),
-    format.errors({stack: true}),
-    format.splat(),
-    format.json()
-  ),
-  defaultMeta: {service: 'funtime-api', env},
-  transports: [axiomTransport],
-  exceptionHandlers: [axiomTransport],
-  rejectionHandlers: [axiomTransport],
+  ],
 });
