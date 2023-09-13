bbe8983c2c1a1438df499df75cf60310ac707d23
Bob Ambrose <bambrose24@gmail.com>
2023-09-12 22:49:40 -0400

    local import for env

--- src/util/logger.ts
+++ src/util/logger.ts
@@ -1,4 +1,4 @@
-import {env} from 'src/config';
+import {env} from '../../src/config';
 import {createLogger, format} from 'winston';
 import {WinstonTransport as AxiomTransport} from '@axiomhq/winston';
 
