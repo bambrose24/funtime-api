4d9d812d489ebfd4f6e1ee44441cfdc510c3ccc1
Bob Ambrose <bambrose24@gmail.com>
2023-09-13 09:26:42 -0400

    ignore introspection query

--- src/graphql/plugins/loggingPlugin.ts
+++ src/graphql/plugins/loggingPlugin.ts
@@ -2,10 +2,12 @@ import {ApolloServerPlugin} from '@apollo/server';
 import {ApolloContext} from '@graphql/server/types';
 import {logger} from '@util/logger';
 
+const IGNORE_OPERATION_NAMES = ['IntrospectionQuery'];
+
 export const loggingPlugin: ApolloServerPlugin<ApolloContext> = {
   requestDidStart({request, contextValue}) {
     return new Promise(resolve => {
-      if (request.operationName) {
+      if (request.operationName && !IGNORE_OPERATION_NAMES.includes(request.operationName)) {
         logger.info(`[graphql] Running operation ${request.operationName}`, {
           operationName: request.operationName,
           query: request.query,
