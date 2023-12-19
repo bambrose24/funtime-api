b3b5b1e5e35f72d13df13154d14dd7789f4313cd
Bob Ambrose <bambrose24@gmail.com>
2023-12-19 08:52:43 -0500

    undo the last season keep updated thing

--- src/cron/keepThingsUpdated.ts
+++ src/cron/keepThingsUpdated.ts
@@ -1,7 +1,6 @@
 import {getGamesBySeason} from '@shared/mysportsfeeds';
 import {SEASON} from '@util/const';
 import {logger} from '@util/logger';
-import moment from 'moment';
 import {markWinners} from './markWinners';
 import updateGamesAndPicks from './updateGamesAndPicks';
 
@@ -12,10 +11,4 @@ export default async function keepThingsUpdated() {
 
   await updateGamesAndPicks(games);
   await markWinners(SEASON);
-
-  // run for 2022 because I messed up and deleted the week 15 winner for league_id 7
-  const gamesOld = await getGamesBySeason(SEASON - 1);
-
-  await updateGamesAndPicks(gamesOld);
-  await markWinners(SEASON - 1);
 }
