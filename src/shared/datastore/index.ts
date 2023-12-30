434f7fbff7e812027b373e4bcfbc01d5fa7bbf26
Bob Ambrose <bambrose24@gmail.com>
2023-12-29 22:40:39 -0500

    dont set the db url manually

--- src/shared/datastore/index.ts
+++ src/shared/datastore/index.ts
@@ -1,14 +1,10 @@
+require('dotenv').config();
 import {PrismaClient} from '@prisma/client';
 import {withAccelerate} from '@prisma/extension-accelerate';
 console.log('SUPABASE_DB_PRISMA ?? ', process.env.SUPABASE_DB_PRISMA);
 const datastore = new PrismaClient({
   // log: ["error", "info", "query", "warn"],
   log: ['error'],
-  datasources: {
-    db: {
-      url: process.env.SUPABASS_DB_PRISMA,
-    },
-  },
 }).$extends(withAccelerate());
 
 // datastore.$use(cacheMiddleware);
